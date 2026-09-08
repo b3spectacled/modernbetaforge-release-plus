0627b7a3be6557a8cdbd60456bf0000c8bd0ac10
b3spectacled <modernbetamc@gmail.com>
2026-09-07 22:51:24 -0700

    Update for Modern Beta 1.10.0.0. Bump major version to 10 to match current Modern Beta version. Add tooltips.

--- src/main/java/mod/bespectacled/modernbetareleaseplus/client/gui/GuiPredicates.java
+++ src/main/java/mod/bespectacled/modernbetareleaseplus/client/gui/GuiPredicates.java
@@ -48,35 +48,35 @@ public class GuiPredicates {
     
     static {
         GENERATOR_VERSION_TEST = new GuiPredicate(settings -> isReleasePlus(settings));
-        BIOME_SIZE_TEST = new GuiPredicate(GENERATOR_VERSION_TEST::test);
+        BIOME_SIZE_TEST = new GuiPredicate(settings -> isReleasePlus(settings));
         
-        OCEAN_TEST = new GuiPredicate(GENERATOR_VERSION_TEST::test);
-        PLAINS_TEST = new GuiPredicate(GENERATOR_VERSION_TEST::test);
-        DESERT_TEST = new GuiPredicate(GENERATOR_VERSION_TEST::test);
-        HILLS_TEST = new GuiPredicate(GENERATOR_VERSION_TEST::test);
-        FOREST_TEST = new GuiPredicate(GENERATOR_VERSION_TEST::test);
-        TAIGA_TEST = new GuiPredicate(GENERATOR_VERSION_TEST::test);
-        SWAMPLAND_TEST = new GuiPredicate(GENERATOR_VERSION_TEST::test);
-        RIVER_TEST = new GuiPredicate(GENERATOR_VERSION_TEST::test);
+        OCEAN_TEST = new GuiPredicate(settings -> isReleasePlus(settings));
+        PLAINS_TEST = new GuiPredicate(settings -> isReleasePlus(settings));
+        DESERT_TEST = new GuiPredicate(settings -> isReleasePlus(settings));
+        HILLS_TEST = new GuiPredicate(settings -> isReleasePlus(settings));
+        FOREST_TEST = new GuiPredicate(settings -> isReleasePlus(settings));
+        TAIGA_TEST = new GuiPredicate(settings -> isReleasePlus(settings));
+        SWAMPLAND_TEST = new GuiPredicate(settings -> isReleasePlus(settings));
+        RIVER_TEST = new GuiPredicate(settings -> isReleasePlus(settings));
         
         FROZEN_RIVER_TEST = new GuiPredicate(settings -> isReleasePlus(settings) && isNewerOrEqualTo(settings, MCVersion.v1_0));
-        FROZEN_OCEAN_TEST = new GuiPredicate(FROZEN_RIVER_TEST::test);
-        ICE_PLAINS_TEST = new GuiPredicate(FROZEN_RIVER_TEST::test);
-        ICE_MOUNTAINS_TEST = new GuiPredicate(FROZEN_RIVER_TEST::test);
-        MUSHROOM_ISLAND_TEST = new GuiPredicate(FROZEN_RIVER_TEST::test);
-        MUSHROOM_ISLAND_SHORE_TEST = new GuiPredicate(FROZEN_RIVER_TEST::test);
+        FROZEN_OCEAN_TEST = new GuiPredicate(settings -> isReleasePlus(settings) && isNewerOrEqualTo(settings, MCVersion.v1_0));
+        ICE_PLAINS_TEST = new GuiPredicate(settings -> isReleasePlus(settings) && isNewerOrEqualTo(settings, MCVersion.v1_0));
+        ICE_MOUNTAINS_TEST = new GuiPredicate(settings -> isReleasePlus(settings) && isNewerOrEqualTo(settings, MCVersion.v1_0));
+        MUSHROOM_ISLAND_TEST = new GuiPredicate(settings -> isReleasePlus(settings) && isNewerOrEqualTo(settings, MCVersion.v1_0));
+        MUSHROOM_ISLAND_SHORE_TEST = new GuiPredicate(settings -> isReleasePlus(settings) && isNewerOrEqualTo(settings, MCVersion.v1_0));
         
         BEACH_TEST = new GuiPredicate(settings -> isReleasePlus(settings) && isNewerOrEqualTo(settings, MCVersion.v1_1));
-        DESERT_HILLS_TEST = new GuiPredicate(BEACH_TEST::test);
-        FOREST_HILLS_TEST = new GuiPredicate(BEACH_TEST::test);
-        TAIGA_HILLS_TEST = new GuiPredicate(BEACH_TEST::test);
-        EXTREME_HILLS_EDGE_TEST = new GuiPredicate(BEACH_TEST::test);
+        DESERT_HILLS_TEST = new GuiPredicate(settings -> isReleasePlus(settings) && isNewerOrEqualTo(settings, MCVersion.v1_1));
+        FOREST_HILLS_TEST = new GuiPredicate(settings -> isReleasePlus(settings) && isNewerOrEqualTo(settings, MCVersion.v1_1));
+        TAIGA_HILLS_TEST = new GuiPredicate(settings -> isReleasePlus(settings) && isNewerOrEqualTo(settings, MCVersion.v1_1));
+        EXTREME_HILLS_EDGE_TEST = new GuiPredicate(settings -> isReleasePlus(settings) && isNewerOrEqualTo(settings, MCVersion.v1_1));
 
         JUNGLE_TEST = new GuiPredicate(settings -> isReleasePlus(settings) && isNewerOrEqualTo(settings, MCVersion.v1_2_1));
-        JUNGLE_HILLS_TEST = new GuiPredicate(JUNGLE_TEST::test);
+        JUNGLE_HILLS_TEST = new GuiPredicate(settings -> isReleasePlus(settings) && isNewerOrEqualTo(settings, MCVersion.v1_2_1));
         
         USE_DEEP_OCEANS_TEST = new GuiPredicate(settings -> isReleasePlus(settings));
         DEEP_OCEAN_CHANCE_TEST = new GuiPredicate(settings -> isReleasePlus(settings) && settings.getBooleanProperty(SettingTags.USE_DEEP_OCEANS));
-        DEEP_OCEAN_NOISE_SCALE_TEST = new GuiPredicate(DEEP_OCEAN_CHANCE_TEST::test);
+        DEEP_OCEAN_NOISE_SCALE_TEST = new GuiPredicate(settings -> isReleasePlus(settings) && settings.getBooleanProperty(SettingTags.USE_DEEP_OCEANS));
     }
 }
